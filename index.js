9d58b8d72673eb61f54c1447a03316c79cf3e73a
mariot <mariotjimenez@gmail.com>
2018-09-15 23:29:42 -0500

    Added use json and liveliness

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const bot_id = "REPLACE THIS WITH YOUR BOT ID ONCE BOT IS ADDED TO THE CHAT"
+
+app.use(express.json());
+
+app.get('/liveliness', (req, res) => res.send('Alive'))
 
 app.listen(PORT, () => console.log('Example app listening on port 3000!'))
