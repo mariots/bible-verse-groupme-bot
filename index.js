5f8013df1d9a9b0afeefc98c132e64d49b5783dd
mariot <mariotjimenez@gmail.com>
2018-09-17 21:07:38 -0500

    Removed book list and added regex

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const axios = require('axios')
+const bibleRegex = require('./bibleRegex')
 
 const bot_id = "6cab5b4f50b68ac9df58f04296"
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('In the beginning, God created the heavens and the earth.'))
+app.get('/', (req, res) => {
+
+    let regex = bibleRegex.books
+    console.log(regex);
+
+    res.send('In the beginning, God created the heavens and the earth.')
+})
 
 app.get('/test-groupme', (req, res) => {
     reply("In the beginning was the Word, and the Word was with God, and the Word was God.")
@@ -28,81 +35,7 @@ app.post('/verse', (req, res) => {
     let message = req.body.text
     console.log(req.body)
 
-    let books = ['genesis', 
-                'exodus',
-                'leviticus',
-                'numbers',
-                'deuteronomy',
-                'joshua',
-                'judges',
-                'ruth',
-                '1 samuel',
-                '2 samuel',
-                '1 kings',
-                '2 kings',
-                '1 chronicles',
-                '2 chronicles',
-                'ezra',
-                'nehemiah',
-                'esther',
-                'job',
-                'psalms',
-                'psalm',
-                'proverbs',
-                'ecclesiastes',
-                'song of solomon',
-                'isaiah',
-                'jeremiah',
-                'lamentations',
-                'ezekiel',
-                'daniel',
-                'hosea',
-                'joel',
-                'amos',
-                'obadiah',
-                'jonah',
-                'micah',
-                'nahum',
-                'habakkuk',
-                'zephaniah',
-                'haggai',
-                'zechariah',
-                'malachi',
-                'matthew',
-                'mark',
-                'luke',
-                'john',
-                'acts',
-                'romans',
-                '1 corinthians',
-                '2 corinthians',
-                'galatians',
-                'ephesians',
-                'philippians',
-                'colossians',
-                '1 thessalonians',
-                '2 thessalonians',
-                '1 timothy',
-                '2 timothy',
-                'titus',
-                'philemon',
-                'hebrews',
-                'james',
-                '1 peter',
-                '2 peter',
-                '1 john',
-                '2 john',
-                '3 john',
-                'jude',
-                'Revelation',
-                'samuel',
-                'kings',
-                'chronicles',
-                'corinthians',
-                'thessalonians',
-                'timothy',
-                'peter',
-                'john']
+    
 
     // TODO search all pieces of text in string, find bible books and pull them out
     let splitMessage = message.toLowerCase().split(" ")
