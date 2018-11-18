17dc24a01a1524c2dda86b7f6b98ecfaeb00cebd
mariot <mariotjimenez@gmail.com>
2018-11-17 23:07:36 -0600

    Fixed linter errors in groupme service

--- service/groupMeService.js
+++ service/groupMeService.js
@@ -1,29 +1,36 @@
+const axios = require('axios');
 
 // Checks whether the message sender is a bot
-function sender_is_bot(message) {
-    return message['sender_type'] == "bot";
+function senderIsBot(message) {
+    return message.sender_type === 'bot';
 }
 
-function reply(msg, bot_id) {
+function reply(msg, botId) {
     const url = 'https://api.groupme.com/v3/bots/post';
 
     const data = {
-        'bot_id': bot_id,
-        'text': msg
-    }
+        'bot_id': botId,
+        'text': msg,
+    };
 
-    return new Promise(function(resolve, reject) {
+    return new Promise((resolve, reject) => {
         axios.post(url, data)
-            .then(function (res) {
+            .then((res) => {
                 // console.log("axios response: ", res.data)
                 // console.log("axios response: ", res)
                 // console.log("VERSE BABAY")
-                console.log(bot_id);
+                console.log(botId);
                 console.log(msg);
                 resolve(res);
-            }).catch(function (err) {
+            }).catch((err) => {
                 // console.log("axios error: ", err.response)
-                console.log("failed", err);
-            })
-    })
+                console.log('failed: ', err);
+                reject(err);
+            });
+    });
 }
+
+module.exports = {
+    senderIsBot,
+    reply,
+};
