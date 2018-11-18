b2982144b6b2c22c9600a15fec8487d5d49d1f72
mariot <mariotjimenez@gmail.com>
2018-11-17 22:46:14 -0600

    Added Linter and linter values

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+    "env": {
+        "es6": true
+    },
+    "rules": {
+        // Indent with 4 spaces
+        'indent': ['error', 4],
+        'quote-props': ["error", "consistent"],
+    }
+};
